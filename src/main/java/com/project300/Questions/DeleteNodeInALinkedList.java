fdcca712c8efafa174d185c7ff58ff6a88adf002
dvc-77 <neilohene@gmail.com>
2024-01-08 22:11:53 +0000

    chore: comment out code for LC

--- src/main/java/com/project300/Questions/DeleteNodeInALinkedList.java
+++ src/main/java/com/project300/Questions/DeleteNodeInALinkedList.java
@@ -1,18 +1,18 @@
-package com.project300.Questions;
-
-public class DeleteNodeInALinkedList {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode(int x) { val = x; }
-     * }
-     */
-    class Solution {
-        public void deleteNode(ListNode node) {
-            node.val = node.next.val;
-            node.next = node.next.next;
-        }
-    }
-}
+//package com.project300.Questions;
+//
+//public class DeleteNodeInALinkedList {
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode(int x) { val = x; }
+//     * }
+//     */
+//    class Solution {
+//        public void deleteNode(ListNode node) {
+//            node.val = node.next.val;
+//            node.next = node.next.next;
+//        }
+//    }
+//}
--- src/main/java/com/project300/Questions/ReverseLinkedList.java
+++ src/main/java/com/project300/Questions/ReverseLinkedList.java
@@ -1,38 +1,38 @@
-package com.project300.Questions;
-
-public class ReverseLinkedList {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
-    class Solution {
-        public ListNode reverseList(ListNode head) {
-            ListNode currentNode = head;
-            ListNode previousNode = null;
-
-            /*
-             *   At each node, set previousNode to replace the nextNode
-             *   Set previousNode to currentNode
-             *   Set currentNode to nextNode
-             *   So for [ <- P - C - N -> ] becomes:
-             *          [ <- N - C - P -> ]
-             *   Where nextNode now points to previousNode in singly LinkedList
-             */
-
-            while(currentNode != null){
-                ListNode nextNode = currentNode.next;
-                currentNode.next = previousNode;
-                previousNode = currentNode;
-                currentNode = nextNode;
-            }
-
-            return previousNode;
-        }
-    }
-}
+//package com.project300.Questions;
+//
+//public class ReverseLinkedList {
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode() {}
+//     *     ListNode(int val) { this.val = val; }
+//     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    class Solution {
+//        public ListNode reverseList(ListNode head) {
+//            ListNode currentNode = head;
+//            ListNode previousNode = null;
+//
+//            /*
+//             *   At each node, set previousNode to replace the nextNode
+//             *   Set previousNode to currentNode
+//             *   Set currentNode to nextNode
+//             *   So for [ <- P - C - N -> ] becomes:
+//             *          [ <- N - C - P -> ]
+//             *   Where nextNode now points to previousNode in singly LinkedList
+//             */
+//
+//            while(currentNode != null){
+//                ListNode nextNode = currentNode.next;
+//                currentNode.next = previousNode;
+//                previousNode = currentNode;
+//                currentNode = nextNode;
+//            }
+//
+//            return previousNode;
+//        }
+//    }
+//}
