14257027fea9ca2e9a375df705b370a3e0fc2212
dvc-77 <neilohene@gmail.com>
2024-01-13 03:49:38 +0000

    update: LC solution format

---
+++ src/main/java/com/project300/Questions/BubbleSort.java
@@ -0,0 +1,15 @@
+package com.project300.Questions;
+
+public class BubbleSort {
+    class Solution {
+
+
+        public int[] bubbleSort(int[] array) {
+            boolean sorted = false;
+            int unsortedUntilIndex = array.length;
+
+            return array;
+        }
+
+    }
+}
