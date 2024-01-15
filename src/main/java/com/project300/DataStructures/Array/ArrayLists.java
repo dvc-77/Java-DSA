8b41b76b9ed71b97df1ab41e0d71f42b39acb3ab
dvc-77 <neilohene@gmail.com>
2024-01-15 16:25:50 +0000

    inserting into an ordered array

--- src/main/java/com/project300/DataStructures/Array/ArrayLists.java
+++ src/main/java/com/project300/DataStructures/Array/ArrayLists.java
@@ -3,14 +3,53 @@ package com.project300.DataStructures.Array;
 import java.util.*;
 
 public class ArrayLists {
+
+
+    // Insertion via Linear search
+    public static ArrayList<Integer> insert(ArrayList<Integer> arr, int value) {
+        int length = arr.size();
+
+        if(value > arr.getLast()) {
+            arr.add(value);
+            return arr;
+        }
+
+        for(int index = 0; index < length; index++) {
+            if(arr.get(index) > value) {
+
+                for(int shiftIndex = length - 1; shiftIndex >= arr.get(index); shiftIndex--) {
+                    int temp = arr.get(shiftIndex);
+                    arr.add(shiftIndex + 1, temp);
+                }
+                arr.add(index, value);
+                break;
+            }
+
+        }
+        return arr;
+    }
+
+    // Insertion via binary search
+
+//    public static ArrayList<In>
+
+
     public static void main(String[] args) {
         int[] intArray = {1, 2, 3, 4, 5};
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(3);
+        arr.add(17);
+        arr.add(80);
+        arr.add(202);
 
-        // Convert int[] to Integer[] using Java 8 Streams
-        Integer[] integerArray = Arrays.stream(intArray).boxed().toArray(Integer[]::new);
+        System.out.println(insert(arr, 75));
+        System.out.println(insert(arr, 90));
 
-        // Create a set from the Integer[] array
-        Set<Integer> set = new HashSet<>(Arrays.asList(integerArray));
+//        // Convert int[] to Integer[] using Java 8 Streams
+//        Integer[] integerArray = Arrays.stream(intArray).boxed().toArray(Integer[]::new);
+//
+//        // Create a set from the Integer[] array
+//        Set<Integer> set = new HashSet<>(Arrays.asList(integerArray));
 
     }
 }
--- src/main/java/com/project300/Questions/IsAnagram.java
+++ src/main/java/com/project300/Questions/IsAnagram.java
@@ -1,18 +1,19 @@
 package com.project300.Questions;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class IsAnagram {
     class Solution {
         public boolean isAnagram(String s, String t) {
             char[] c1 = s.toCharArray();
             char[] c2 = t.toCharArray();
-
+            Integer c = 0;
             Arrays.sort(c1);
             Arrays.sort(c2);
-
+            HashSet<Integer> l = new HashSet<>();
             return Arrays.equals(c1, c2);
-
         }
     }
 }
